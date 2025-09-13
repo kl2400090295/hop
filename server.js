6d3188b95cdf6660eea29198654eae5ec052ecca
Santosh Chandu <chs@Santoshs-MacBook-Air.local>
2025-09-13 23:58:23 +0530

    Update server.js and add vercel.json for deployment

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+module.exports = app;
